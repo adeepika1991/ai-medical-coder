536e0dec4a4af0686350605f02e3ea39589235b8
deepika <adeepika1991@gmail.com>
2025-08-01 23:43:16 -0400

    fix: removed test endpoint

--- src/app/api/ping/route.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  const ping = await prisma.ping.create({ data: { text: 'pong from db' } });
-  return NextResponse.json(ping);
-}
